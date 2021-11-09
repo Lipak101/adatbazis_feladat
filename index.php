84df5ef51bc4077d6067f3cbd0295dd9daa7465f
Szabó Péter <szpeter090999@gmail.com>
2021-11-09 14:23:13 +0100

    SzabóPéter

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+print("
+<head>
+<link rel='stylesheet' href='style.css'>
+</head>
+<body>
+<video autoplay loop id='myVideo'>
+  <source src='xue.mp4' type='video/mp4'>
+</video>
+</body>
+<ul>
+<li><a href='bovit.php'>Bővít</a></li>
+<li><a href='torol.php'>Töröl</a></li>
+<li><a href='modosit.php'>Módosít</a></li>
+<li><a href='listaz.php'>Listázás</a></li>
+</ul>");
+
+?>
