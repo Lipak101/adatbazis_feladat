791e3f551a858b41c50fda3b75ec56811c69583b
Szabó Péter <szpeter090999@gmail.com>
2021-11-09 14:24:36 +0100

    SzabóPéter

---
+++ listaz.php
@@ -0,0 +1,25 @@
+<?php
+
+include("index.php");
+
+require("kapcs.inc.php");
+
+if(!isset($_GET['adat'])){
+
+$eredm = mysqli_query($con,"SELECT * FROM kepek") or die ("Nem sikerült a lekérdezés!");
+
+
+while($rekord = mysqli_fetch_object($eredm)){
+    print("<form action='' method='GET'>");
+    print("<br><a href=listaz.php?adat=".$rekord->id.">".$rekord->id."</a>");
+    print("<br>".$rekord->kod);
+    print("<br>".$rekord->nev);
+    print("<br>".$rekord->mennyiseg);
+    print("<br>".$rekord->egysegar);
+    print("<br>".$rekord->kedvar);
+    print("</form>");
+}
+}
+
+
+?>
