d1f68880d77bc9d5d702b0df08bcc2f31d0021a1
Szabó Péter <szpeter090999@gmail.com>
2021-11-09 14:24:08 +0100

    SzabóPéter

---
+++ legordulo.php
@@ -0,0 +1,11 @@
+<?php
+    require("kapcs.inc.php");
+for($i=0;$i<10;$i++){
+    $query = ("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'csapat2xd' AND TABLE_NAME = 'kepek';");    
+
+}
+mysqli_connect($rekord,$query);
+while(){
+    
+}
+?>
